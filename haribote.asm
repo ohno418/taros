be43cd1ab4c9f119558cfd0e29a1b395bd378401
yutarofire <yutaro.ono.418@gmail.com>
2021-02-24 23:15:50 +0900

    Use keyboard interruption

--- haribote.asm
+++ haribote.asm
@@ -1,8 +1,13 @@
   org 0xc200
 
+  ; Video interrupt
   mov al, 0x13 ; Standard 256-color mode on VGA graphics hardware
   mov ah, 0x00 ; Video mode
   int 0x10
+
+  ; Keyboard interrupt
+  mov ah, 0x02 ; Get the state of the keyboard
+  int 0x16
 
 fin:
   hlt
