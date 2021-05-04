bb35cef239a73a47f4f43b2dc4be41f2a7b022de
yutarofire <yutaro.ono.418@gmail.com>
2021-05-04 18:22:33 +0900

    Use clang instead of gcc

---
+++ kernel/libcxx_support.cpp
@@ -0,0 +1,18 @@
+#include <new>
+#include <cerrno>
+
+char* buf;
+void* operator new(unsigned long size) {
+  return buf;
+}
+
+void operator delete(void* buf, std::align_val_t val) {
+}
+
+std::new_handler std::get_new_handler() noexcept {
+  return nullptr;
+}
+
+extern "C" int posix_memalign(void**, size_t, size_t) {
+  return ENOMEM;
+}
