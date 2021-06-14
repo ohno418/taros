a67884a8c229940fa9aab172b914d1b185e42271
yutarofire <yutaro.ono.418@gmail.com>
2021-06-14 21:08:11 +0900

    Setup paging

---
+++ kernel/paging.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cstddef>
+
+const size_t kPageDirectoryCount = 64;
+
+/** @brief Setup the page table that virtual address = physical address.
+ */
+void SetupIdentityPageTable();
