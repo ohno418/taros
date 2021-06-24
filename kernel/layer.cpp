d7e6162d88f8281805d87eb80b7dcba8059c6ba2
yutarofire <yutaro.ono.418@gmail.com>
2021-06-24 21:40:43 +0900

    Add LayerManager

--- kernel/layer.cpp
+++ kernel/layer.cpp
@@ -1,4 +1,5 @@
 #include "layer.hpp"
+#include <algorithm>
 
 Layer::Layer(unsigned int id) : id_{id} {
 }
@@ -31,3 +32,70 @@ void Layer::DrawTo(PixelWriter& writer) const {
     window_->DrawTo(writer, pos_);
   }
 }
+
+
+void LayerManager::SetWriter(PixelWriter* writer) {
+  writer_ = writer;
+}
+
+Layer& LayerManager::NewLayer() {
+  ++latest_id_;
+  return *layers_.emplace_back(new Layer{latest_id_});
+}
+
+void LayerManager::Draw() const {
+  for (auto layer : layer_stack_) {
+    layer->DrawTo(*writer_);
+  }
+}
+
+void LayerManager::Move(unsigned int id, Vector2D<int> new_position) {
+  FindLayer(id)->Move(new_position);
+}
+
+void LayerManager::MoveRelative(unsigned int id, Vector2D<int> pos_diff) {
+  FindLayer(id)->MoveRelative(pos_diff);
+}
+
+void LayerManager::UpDown(unsigned int id, int new_height) {
+  if (new_height < 0) {
+    Hide(id);
+    return;
+  }
+  if (new_height > layer_stack_.size()) {
+    new_height = layer_stack_.size();
+  }
+  auto layer = FindLayer(id);
+  auto old_pos = std::find(layer_stack_.begin(), layer_stack_.end(), layer);
+  auto new_pos = layer_stack_.begin() + new_height;
+  if (old_pos == layer_stack_.end()) {
+    layer_stack_.insert(new_pos, layer);
+    return;
+  }
+  if (new_pos == layer_stack_.end()) {
+    --new_pos;
+  }
+  layer_stack_.erase(old_pos);
+  layer_stack_.insert(new_pos, layer);
+}
+
+void LayerManager::Hide(unsigned int id) {
+  auto layer = FindLayer(id);
+  auto pos = std::find(layer_stack_.begin(), layer_stack_.end(), layer);
+  if (pos != layer_stack_.end()) {
+    layer_stack_.erase(pos);
+  }
+}
+
+Layer* LayerManager::FindLayer(unsigned int id) {
+  auto pred = [id](const std::unique_ptr<Layer>& elem) {
+    return elem->ID() == id;
+  };
+  auto it = std::find_if(layers_.begin(), layers_.end(), pred);
+  if (it == layers_.end()) {
+    return nullptr;
+  }
+  return it->get();
+}
+
+LayerManager* layer_manager;
--- kernel/layer.hpp
+++ kernel/layer.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <memory>
+#include <map>
 #include <vector>
+#include "graphics.hpp"
 #include "window.hpp"
 
 class Layer {
@@ -22,3 +24,34 @@ class Layer {
   Vector2D<int> pos_;
   std::shared_ptr<Window> window_;
 };
+
+class LayerManager {
+ public:
+   void SetWriter(PixelWriter* writer);
+   Layer& NewLayer();
+
+   /** @brief Draw layers currently displayable. */
+   void Draw() const;
+
+   void Move(unsigned int id, Vector2D<int> new_position);
+   void MoveRelative(unsigned int id, Vector2D<int> pos_diff);
+   /** @brief Change layer height.
+    *
+    * Negative new_height makes the layer not display.
+    * Positive new_height makes the layer specified height.
+    * If new_height is larger than current layer height, the layer will be foremost.
+    */
+   void UpDown(unsigned int id, int new_height);
+   /** @brief Make a layer not display. */
+   void Hide(unsigned int id);
+
+ private:
+  PixelWriter* writer_{nullptr};
+  std::vector<std::unique_ptr<Layer>> layers_{};
+  std::vector<Layer*> layer_stack_{};
+  unsigned int latest_id_{0};
+
+  Layer* FindLayer(unsigned int id);
+};
+
+extern LayerManager* layer_manager;
