2ee17829eeb1ba3e933e88ed1ae9f16ea13478d8
yutarofire <yutaro.ono.418@gmail.com>
2021-06-26 13:11:53 +0900

    Change detail

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -98,8 +98,8 @@ void IntHandlerXHCI(InterruptFrame* frame) {
 alignas(16) uint8_t kernel_main_stack[1024 * 1024];
 
 extern "C" void KernelMainNewStack(
-  const FrameBufferConfig& frame_buffer_config_ref,
-  const MemoryMap& memory_map_ref) {
+    const FrameBufferConfig& frame_buffer_config_ref,
+    const MemoryMap& memory_map_ref) {
   FrameBufferConfig frame_buffer_config{frame_buffer_config_ref};
   MemoryMap memory_map{memory_map_ref};
 
