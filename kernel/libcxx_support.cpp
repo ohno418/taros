c81e6191fd0de65d0915419ebde770a1a30cd4e3
taro0 <yutaro.ono.418@gmail.com>
2021-07-04 13:34:42 +0900

    fix: Link libc++ and etc

--- kernel/libcxx_support.cpp
+++ kernel/libcxx_support.cpp
@@ -12,9 +12,9 @@ void operator delete(void* buf, std::align_val_t val) {
 void operator delete(void* obj) noexcept {
 }
 
-std::new_handler std::get_new_handler() noexcept {
-  return nullptr;
-}
+// std::new_handler std::get_new_handler() noexcept {
+//   return nullptr;
+// }
 
 extern "C" int posix_memalign(void**, size_t, size_t) {
   return ENOMEM;
--- kernel/newlib_support.c
+++ kernel/newlib_support.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stddef.h>
+#include <stdio.h>
 
 caddr_t program_break, program_break_end;
 
@@ -51,3 +52,53 @@ int fstat(int file, struct stat *st) {
 int isatty(int file) {
   return 0;
 }
+
+int sched_yield(void) {
+  return 0;
+}
+
+FILE *stderr;
+
+int __vfprintf_chk(FILE * fp, int flag, const char * format, va_list ap) {
+  return 0;
+}
+
+void __assert_fail(const char * assertion, const char * file, unsigned int line, const char * function) {}
+const unsigned short * * __ctype_b_loc (void) {
+  return 0;
+}
+
+int dl_iterate_phdr(
+          int (*callback) (struct dl_phdr_info *info,
+                           size_t size, void *data),
+          void *data) {
+  return 0;
+}
+
+void __libc_fatal (const char *message) {}
+
+int __sched_get_priority_max (int __algorithm) {
+  return 0;
+}
+
+int __sched_get_priority_min (int __algorithm) {
+  return 0;
+}
+
+int __sched_setscheduler (__pid_t __pid, int __policy, const struct sched_param *__param) {
+  return 0;
+}
+
+int __sched_getparam (__pid_t __pid, struct sched_param *__param) {
+  return 0;
+}
+
+int __sched_getscheduler (__pid_t __pid) {
+  return 0;
+}
+
+#define tunable_id_t int
+#define tunable_callback_t int
+void __tunable_get_val (tunable_id_t a, void * b, tunable_callback_t c) {}
+void _dl_x86_cpu_features() {}
+void __libc_longjmp() {}
