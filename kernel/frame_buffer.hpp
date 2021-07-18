eaa74158c90a401b38a5a2f071a7ca4cbd272d6e
taro0 <yutaro.ono.418@gmail.com>
2021-07-18 14:50:38 +0900

    Improve scroll speed

--- kernel/frame_buffer.hpp
+++ kernel/frame_buffer.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <memory>
+
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
 #include "error.hpp"
@@ -9,7 +10,8 @@
 class FrameBuffer {
  public:
   Error Initialize(const FrameBufferConfig& config);
-  Error Copy(Vector2D<int> pos, const FrameBuffer& src);
+  Error Copy(Vector2D<int> dst_pos, const FrameBuffer& src);
+  void Move(Vector2D<int> dst_pos, const Rectangle<int>& src);
 
   FrameBufferWriter& Writer() { return *writer_; }
 
@@ -18,5 +20,3 @@ class FrameBuffer {
   std::vector<uint8_t> buffer_{};
   std::unique_ptr<FrameBufferWriter> writer_{};
 };
-
-int BitsPerPixel(PixelFormat format);
