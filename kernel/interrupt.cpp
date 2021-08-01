a75e88c716ed95cc63f8a55ad66c6f2fd79473d8
taro0 <yutaro.ono.418@gmail.com>
2021-08-01 16:55:08 +0900

    Slower timer

--- kernel/interrupt.cpp
+++ kernel/interrupt.cpp
@@ -1,6 +1,8 @@
 #include "interrupt.hpp"
+
 #include "asmfunc.h"
 #include "segment.hpp"
+#include "timer.hpp"
 
 std::array<InterruptDescriptor, 256> idt;
 
@@ -31,7 +33,7 @@ namespace {
 
   __attribute__((interrupt))
   void IntHandlerLAPICTimer(InterruptFrame* frame) {
-    msg_queue->push_back(Message{Message::kInterruptLAPICTimer});
+    LAPICTimerOnInterrupt();
     NotifyEndOfInterrupt();
   }
 }
